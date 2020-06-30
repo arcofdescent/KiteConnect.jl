1c0ef5312e6a9dcd38f6e196d8d8aba2fd6518d0
Rohan Almeida <rohan.almeida@gmail.com>
2020-07-01 00:16:41 +0530

    http get to api endpoint

--- src/KiteConnect.jl
+++ src/KiteConnect.jl
@@ -1,9 +1,27 @@
+"""
+    KiteConnect
+
+Julia module to interface with Zerodha's KiteConnect API
+"""
 module KiteConnect
+
+using HTTP
+using JSON
+
+const API_ENDPOINT = "https://api.kite.trade"
+const API_KEY = ""
+const API_SECRET = ""
+const ACCESS_TOKEN = ""
 
 include("quote.jl")
 
-# function ()
-#     return 10
-# end
+function authenticate(api_key::String, api_secret::String)
+
+end
+
+function http_get(url::String)
+  r = HTTP.request("GET", "$API_ENDPOINT/quote/ltp")
+  r.body |> String |> JSON.parse
+end
 
 end # module
--- src/quote.jl
+++ src/quote.jl
@@ -1,5 +1,10 @@
 module Quote
 
+"""
+    KiteConnect.Quote.ltp("NSE:INFY")
+
+Get LTP of trading symbol
+"""
 function ltp()
   return 100.56
 end
