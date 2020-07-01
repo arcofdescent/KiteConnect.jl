186289f09c0c86a89152f1de2aa5b38498a936e1
Rohan Almeida <rohan.almeida@gmail.com>
2020-07-01 23:45:27 +0530

    working on generating access token

--- src/KiteConnect.jl
+++ src/KiteConnect.jl
@@ -7,21 +7,40 @@ module KiteConnect
 
 using HTTP
 using JSON
+using SHA
 
 const API_ENDPOINT = "https://api.kite.trade"
-const API_KEY = ""
-const API_SECRET = ""
-const ACCESS_TOKEN = ""
+API_KEY = ""
+API_SECRET = ""
+ACCESS_TOKEN = ""
 
 include("quote.jl")
 
 function authenticate(api_key::String, api_secret::String)
-
+  API_KEY = api_key
+  API_SECRET = api_secret
 end
 
 function http_get(url::String)
-  r = HTTP.request("GET", "$API_ENDPOINT/quote/ltp")
-  r.body |> String |> JSON.parse
+  println(API_SECRET)
+  # r = HTTP.request("GET", "$API_ENDPOINT/quote/ltp")
+  # r.body |> String |> JSON.parse
+end
+
+function gen_access_token(request_token::String)
+  checksum = request_token |> sha256 |> bytes2hex
+  println(checksum)
+  url = "$API_ENDPOINT/session/token"
+
+  headers = [
+    "X-Kite-Version" => "3",
+    "Content-Type"   => "application/x-www-form-urlencoded",
+  ]
+
+  body = "api_key=$API_KEY&request_token=$request_token&checksum=$checksum"
+  println(body)
+
+  HTTP.request("POST", url, headers, body)
 end
 
 end # module
