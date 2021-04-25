d83a10cf49091e94efb63229146e805e74880e9f
Rohan Almeida <rohan.almeida@gmail.com>
2021-04-25 11:46:10 +0530

    updated HTTP, using JSON3

--- src/KiteConnect.jl
+++ src/KiteConnect.jl
@@ -5,11 +5,11 @@ Julia module to interface with Zerodha's KiteConnect API
 """
 module KiteConnect
 
-export init, gen_access_token
-
 using HTTP
-using JSON
+using JSON3
 using SHA
+
+include("quote.jl")
 
 const API_ENDPOINT = "https://api.kite.trade"
 API_KEY = ""
@@ -23,21 +23,21 @@ Setup your KiteConnect session by providing your API key and
 API secret which you get from Zerodha
 """
 function init(api_key::String, api_secret::String)
-  global API_KEY = api_key
-  global API_SECRET = api_secret
+    global API_KEY = api_key
+    global API_SECRET = api_secret
 end
 
 function get_http_headers()
-  [
-    "X-Kite-Version" => "3",
-    "Authorization" => "token $API_KEY:$ACCESS_TOKEN",
-  ]
+    return [
+        "X-Kite-Version" => "3",
+        "Authorization" => "token $API_KEY:$ACCESS_TOKEN",
+    ]
 end
 
 function http_get(url_fragment::String)
-  url = "$API_ENDPOINT/$url_fragment"
-  r = HTTP.request("GET", url, get_http_headers())
-  r.body |> String |> JSON.parse
+    url = "$API_ENDPOINT/$url_fragment"
+    r = HTTP.request("GET", url, get_http_headers())
+    return r.body |> String |> JSON3.read
 end
 
 """
@@ -46,21 +46,18 @@ end
 Generate the access token by passing in yout request token
 """
 function gen_access_token(request_token::String)
-  checksum = (API_KEY * request_token * API_SECRET) |> sha256 |> bytes2hex
-  url = "$API_ENDPOINT/session/token"
+    checksum = (API_KEY * request_token * API_SECRET) |> sha256 |> bytes2hex
+    url = "$API_ENDPOINT/session/token"
 
-  headers = [
-    "X-Kite-Version" => "3",
-    "Content-Type"   => "application/x-www-form-urlencoded",
-  ]
-  body = "api_key=$API_KEY&request_token=$request_token&checksum=$checksum"
+    headers = [
+        "X-Kite-Version" => "3",
+        "Content-Type"   => "application/x-www-form-urlencoded",
+    ]
+    body = "api_key=$API_KEY&request_token=$request_token&checksum=$checksum"
 
-  res = HTTP.request("POST", url, headers, body)
-  r = res.body |> String |> JSON.parse
-  global ACCESS_TOKEN = r["data"]["access_token"]
+    res = HTTP.request("POST", url, headers, body)
+    r = res.body |> String |> JSON3.read
+    global ACCESS_TOKEN = r["data"]["access_token"]
 end
-
-include("quote.jl")
-export ltp
 
 end # module
--- src/quote.jl
+++ src/quote.jl
@@ -7,13 +7,14 @@ Get LTP of trading symbol
 `instrument` should be in EXCHANGE:TRADINGSYMBOL format, eg. "NSE:INFY"
 """
 function ltp(instrument::String)
-  parts = split(instrument, ":")
-  if (length(parts) != 2)
-    throw(ArgumentError("instrument should be in EXCHANGE:TRADINGSYMBOL format"))
-  end
+    parts = split(instrument, ":")
+    if (length(parts) != 2)
+        throw(ArgumentError("instrument should be in EXCHANGE:TRADINGSYMBOL format"))
+    end
 
-  url_fragment = "quote/ltp?i=$instrument"
-  res = http_get(url_fragment)
-  res["data"][instrument]["last_price"]
+    url_fragment = "quote/ltp?i=$instrument"
+    res = http_get(url_fragment)
+
+    return res["data"][instrument]["last_price"]
 end
 
